5e4fb3aee484ad7de96429f3c73163c85a89138c
sriramrayavalasa <sriramrayavalasa@gmail.com>
2025-03-28 14:32:29 +0530

    updated

---
+++ 27/object.js
@@ -0,0 +1,22 @@
+let cars=[
+
+    {
+        names:"bmw",
+        price:12000,
+        Color:"red" 
+    },
+    {
+        names:"audi",
+        price:12000,
+        Color:"white" 
+    }, {
+        names:"rollsroyals",
+        price:12000,
+        Color:"gold" 
+    }, {
+        names:"thor",
+        price:12000,
+        Color:"black" 
+    },
+]
+console.log(cars)
